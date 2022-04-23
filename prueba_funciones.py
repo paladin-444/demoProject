e71c9aaecfbf7737e0c3808fffb0a1ab5d7ed183
paladin-444 <valentina24miranda@gmail.com>
2022-04-22 20:54:07 -0500

    first commit

---
+++ prueba_funciones.py
@@ -0,0 +1,12 @@
+#largest_so_far= -1
+#for i in [3, 41, 12, 9, 74, 15]:
+#    if i > largest_so_far:
+#        largest_so_far=i
+#print(largest_so_far)
+from re import X
+
+
+x = None
+y = 3
+if y > x:
+    print('idk')
